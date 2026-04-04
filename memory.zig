f8b48471c48c51b32201784db01c5ad53c816810
Wrethr <112186856+Wrethr@users.noreply.github.com>
2026-04-04 10:36:21 +0700

    Update memory.zig

--- memory.zig
+++ memory.zig
@@ -1,8 +1,3 @@
-const std = @import("std");
-
-const TTY_OUTPUT_ADDR = 0o177566;
-const TTY_INPUT_ADDR = 0o177560;
-
 pub const Memory = struct {
     ram: [65536]u8 = [_]u8{0} ** 65536,
 
@@ -12,7 +7,6 @@ pub const Memory = struct {
         const lowByte = self.ram[address];
         const highByte = self.ram[address + 1];
 
-        // литл эндиан, по этому сначала lowByte
         return @as(u16, lowByte) | (@as(u16, highByte) << 8);
     }
 
@@ -22,10 +16,6 @@ pub const Memory = struct {
 
         self.ram[address] = lowByte;
         self.ram[address + 1] = highByte;
-
-        if (address == TTY_OUTPUT_ADDR) {
-            const char = @as(u8, @truncate(value));
-            std.debug.print("{c}", .{char});
-        }
     }
 };
+
